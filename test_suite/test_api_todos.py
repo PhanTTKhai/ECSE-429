b1f498ef396172ebf88320aeeac3941518befbcc
PhanTTKhai <tran.t.phan@mail.mcgill.ca>
2024-10-06 19:07:14 -0400

    Added more tests

--- test_suite/test_api_todos.py
+++ test_suite/test_api_todos.py
@@ -13,13 +13,19 @@ class TestAPITodos(unittest.TestCase):
         "Content-Type": "application/xml"
     }
     created_todo_ids = []
+    created_categories_ids = []
 
     def tearDown(self):
         """ Delete todos created for the test """
         for todo_id in self.created_todo_ids:
             response = requests.delete(f"{self.BASE_URL}/todos/{todo_id}")
             self.assertEqual(200, response.status_code)
+
+        for category_id in self.created_categories_ids:
+            response = requests.delete(f"{self.BASE_URL}/categories/{category_id}")
+            self.assertEqual(200, response.status_code)
         self.created_todo_ids.clear()
+        self.created_categories_ids.clear()
 
     def create_todo(self, data):
         response = requests.post(f"{self.BASE_URL}/todos", json=data)
@@ -33,6 +39,19 @@ class TestAPITodos(unittest.TestCase):
                 raise Exception("Unexpected response format or missing 'id' field")
         else:
             raise Exception(f"Failed to create todo: {response.status_code}")
+
+    def create_category(self, data):
+        response = requests.post(f"{self.BASE_URL}/categories", json=data)
+
+        if response.status_code == 201:
+            try:
+                category_id = response.json()['id']
+                self.created_categories_ids.append(category_id)
+                return int(category_id)
+            except (KeyError, ValueError):
+                raise Exception("Unexpected response format or missing 'id' field")
+        else:
+            raise Exception(f"Failed to create category: {response.status_code}")
 
     def test_get_todos_json(self):
         for i in range(1, 4):
@@ -89,6 +108,103 @@ class TestAPITodos(unittest.TestCase):
 
         response = requests.delete(f"{self.BASE_URL}/todos", json=delete_data, headers=self.json_headers)
         self.assertEqual(405, response.status_code, "Expected 405 Method Not Allowed when sending a DELETE with a body")
+
+    def test_post_link_todo_to_category(self):
+        todo_data = {
+            "title": "Test Todo",
+        }
+        category_data = {
+            "title": "Test category",
+        }
+        todo_id = self.create_todo(todo_data)
+        category = self.create_category(category_data)
+
+        todo_category_link_data = {
+            "id": str(category),
+        }
+
+        response = requests.post(f"{self.BASE_URL}/todos/{todo_id}/categories", json=todo_category_link_data,
+                                 headers=self.json_headers)
+        self.assertEqual(201, response.status_code)
+
+    def test_get_categories_related_to_todo_documented(self):
+        # This test is expected to fail, because it doesn't see the relationship created from category side
+        todo_data = {
+            "title": "Test Todo",
+        }
+        category_data_1 = {
+            "title": "Test category 1",
+        }
+
+        category_data_2 = {
+            "title": "Test category 2",
+        }
+
+        todo_id = self.create_todo(todo_data)
+        category_1 = self.create_category(category_data_1)
+        category_2 = self.create_category(category_data_2)
+
+        todo_category_link_data = {
+            "id": str(category_1),
+        }
+
+        category_data_link_data = {
+            "id": str(todo_data),
+        }
+        # first link
+        requests.post(f"{self.BASE_URL}/todos/{todo_id}/categories", json=todo_category_link_data,
+                      headers=self.json_headers)
+        # second link
+        requests.post(f"{self.BASE_URL}/categories/{category_2}/todos", json=category_data_link_data,
+                      headers=self.json_headers)
+
+        response = requests.get(f"{self.BASE_URL}/todos/{todo_id}/categories", headers=self.json_headers)
+
+        categories = response.json()['categories']
+
+        category_ids = {str(category_1), str(category_2)}
+        returned_category_ids = {category['id'] for category in categories}
+        self.assertEqual(category_ids, returned_category_ids, "The returned categories do not match the expected ones")
+
+    def test_get_categories_related_to_todo_observed(self):
+        # This test is expected to fail, because it doesn't see the relationship created from category side
+        todo_data = {
+            "title": "Test Todo",
+        }
+        category_data_1 = {
+            "title": "Test category 1",
+        }
+
+        category_data_2 = {
+            "title": "Test category 2",
+        }
+
+        todo_id = self.create_todo(todo_data)
+        category_1 = self.create_category(category_data_1)
+        category_2 = self.create_category(category_data_2)
+
+        todo_category_link_data_1 = {
+            "id": str(category_1),
+        }
+
+        todo_category_link_data_2 = {
+            "id": str(category_2),
+        }
+
+        # first link
+        requests.post(f"{self.BASE_URL}/todos/{todo_id}/categories", json=todo_category_link_data_1,
+                      headers=self.json_headers)
+        # second link
+        requests.post(f"{self.BASE_URL}/todos/{todo_id}/categories", json=todo_category_link_data_2,
+                      headers=self.json_headers)
+
+        response = requests.get(f"{self.BASE_URL}/todos/{todo_id}/categories", headers=self.json_headers)
+
+        categories = response.json()['categories']
+
+        category_ids = {str(category_1), str(category_2)}
+        returned_category_ids = {category['id'] for category in categories}
+        self.assertEqual(category_ids, returned_category_ids, "The returned categories do not match the expected ones")
 
     def test_get_todos_xml(self):
         for i in range(1, 4):
